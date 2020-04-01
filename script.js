faf8001917ab2753988576ac39eea421ffec2205
kentaylor92 <ken_seaway@gmail.com>
2020-04-01 13:57:06 -0400

    added tip calculator functionality and base html

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    
+
+  
+
+    function tipCalculator() {
+        
+        const billTotal = $("#billInput").val();
+        const tipPercent = $("#tipPercent").val();
+        
+        
+        const tip = billTotal * (tipPercent / 100);
+        const total = +billTotal + tip;
+
+        $("#tip").text(tip.toFixed(2));
+        $("#total").text(total.toFixed(2));
+    };
+
+    
+
+
+
+    $('#calculate').on('click', function(e) {
+        e.preventDefault();
+        tipCalculator();
+    })
+
+    
+    
+});
